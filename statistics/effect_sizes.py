67c5162b7c695f6f6c8434699a009d1994f02fde
lcdunne <56237933+lcdunne@users.noreply.github.com>
2020-04-05 22:43:07 +0100

    Update effect_sizes.py
    Function updated to allow summary stats as inputs.

--- statistics/effect_sizes.py
+++ statistics/effect_sizes.py
@@ -1,124 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 24 18:39:58 2020
+Created on Wed Mar 25 10:47:12 2020
+
 @author: L
+
+there are at least 5 different and non-equivalent ways that people might 
+compute a d-like effect size (which they would invariably simply call “Cohen’s d”) 
+... and the resulting effect sizes range from about 0.25 to 1.91.
+
+Useful links
+https://cran.r-project.org/web/packages/effectsize/effectsize.pdf
+http://jakewestfall.org/blog/index.php/2016/03/25/five-different-cohens-d-statistics-for-within-subject-designs/
+http://jeffrouder.blogspot.com/2016/03/the-effect-size-puzzler-answer.html
+https://forum.cogsci.nl/discussion/3013/what-denominator-does-the-cohens-d-use-on-jasp
+https://www.uv.es/uvetica/files/Cunningham_McCrum_Gardner2007.pdf
+https://github.com/mtorchiano/effsize/blob/master/R/CohenD.R
 """
 
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+from statsmodels.stats import weightstats as sms
 from statsmodels.stats.anova import AnovaRM
 from statsmodels.formula.api import ols
-from scipy.stats import norm
+from scipy import stats
 
-'''
-Hedge's g involves much of the same computation as cohen's d, and even uses
-Cohen's d in its calculation...
-    Separate the function into two with repetitive code?
-    Calculate both in a more vague function?
-'''
 
-# def _effect_size_ind() <-- to be used in ttest_ind()?
 
-def standardised_cohens_d_hedges_g(x1=None, x2=None, t=None, nobs_x1=None, nobs_x2=None, N=None):
-    '''
-    Computes the standardised Cohen's d effect size statistic for independent samples
-    using the pooled standard deviation.
-    
-    Cliff delta would be the alternative for nonparametric
-    Parameters
-    ----------
-    x1 : array-like, optional
-        first of the two independent samples. The default is None.
-    x2 : array-like, optional
-        second of the two independent samples. The default is None.
-    tstat : scalar, optional
-        t-statistic result from independent t-test. The default is None.
-    nobs_x1 : scalar, optional
-        number of observations for the first of the two independent samples. The default is None.
-    nobs_x2 : scalar, optional
-        number of observations for the second of the two independent samples. The default is None.
-    N : scalar, optional
-        total N (sum of both independent samples). 
-        Note that if only `tstat` and `N` are provided, the standardised 
-        Cohen's d is an approximate. The default is None.
-    Raises
-    ------
-    ValueError
-        When x1 and x2 are None, if any of the following are true:
-            t == None
-            (nobs_x1 == None) and (nobs_x2 == None) and (N == None)
-    Returns
-    -------
-    cohens_d : float
-        Standardised Cohen's d statistic.
-    '''
-    # if x1 and x2 are provided - compute d from this.
-    if (x1 and x2):
-        x1, x2 = np.array(x1), np.array(x2) # Ensure np.array
-        nobs_x1, nobs_x2 = len(x1), len(x2)
-        S_x1, S_x2 = np.std(x1, ddof=1), np.std(x2, ddof=1)
-        dof = nobs_x1 + nobs_x2 - 2 # compute degree of freedom
-        meandiff = np.mean(x1) - np.mean(x2)
-        pooled_sd = np.sqrt( ((nobs_x1 - 1) * S_x1 ** 2 + (nobs_x2 - 1) * S_x2 ** 2) / dof )
-        cohens_d = meandiff / pooled_sd
-        hedges_g = cohens_d * (1 - (3 / (4 * (nobs_x1 + nobs_x2) - 9)))
-        
-    elif (not x1 and not x2):
-        # neither were given, so can we calculate from t and n obs?
-        if t==None:
-            raise ValueError("If `x1` and `x2` are not provided, `t` is needed to compute Cohen's d but was not provided.")
-        else:
-            # We have the t value
-            # Do we have both observation counts?
-            if not nobs_x1 and not nobs_x2:
-                if not N:
-                    raise ValueError("Either `N` or both of `nobs_x1` & `nobs_x2` is required but was not provided.")
-                else:
-                    # Estimating
-                    cohens_d = 2 * t / np.sqrt(N)
-                    hedges_g = cohens_d * (1 - (3 / (4 * N - 9)))
-            else:
-                # We have both n obs
-                cohens_d = t * np.sqrt( (1 / nobs_x1) + (1 / nobs_x2) )
-                hedges_g = cohens_d * (1 - (3 / (4 * (nobs_x1 + nobs_x2) - 9)))
-        
-    return cohens_d, hedges_g
-
-def common_language_effect_size(x1=None, x2=None):
-    '''
-    Common language effect size McGraw & Wong (1992) reflects the probability 
-    that a randomly sampled observation from one sample will have a higher 
-    observed measurement than a randomly sampled observation from the other 
-    sample, either between or within.
-    Parameters
-    ----------
-    x1 : array-like, optional
-        first of the two independent samples. If `x2` not specified, `x1` is 
-        inferred to be a vector representing the difference between the two 
-        samples. The default is None.
-    x2 : array-like, optional
-        second of the two independent samples. The default is None.
-    Raises
-    ------
-    ValueError
-        When no inputs are given.
-    Returns
-    -------
-    CLES : float
-        The common language effect size.
-    '''
-    if (x1==None and x2 == None):
-        raise ValueError("Requires `x1` and `x2`, or difference between them as `x1`, but nothing was provided.")
-    elif x2 == None:
-        # We assume related samples and that x1 represents the difference values
-        Z = np.mean(x1) / np.std(x1, ddof=1)
-    else:
-        cohens_d, _ = standardised_cohens_d_hedges_g(x1, x2)
-        Z = cohens_d / np.sqrt(2)
-        
-    CLES = norm.cdf(Z)
-    return CLES
 
 #~Testing Dataset ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Artificial movie ratings dataset
@@ -133,27 +41,104 @@ print(df, '\n')
 nobs_x1, nobs_x2 = np.size(x1), np.size(x2)
 
 
-#~ Independent Groups ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-t, p, dof = sm.stats.ttest_ind(x1, x2)
-print(f"t({dof}) = {t:.2f}, p = {p:.2f}\n")
+#~ Dependent Groups / Within Samples ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+def cohens_d(x1=None, x2=None, S_x1=None, S_x2=None, ind=True, r=None, hedges_correction=False, nobs_x1=None, nobs_x2=None):
+    
+    realval_check = (int, float, np.float16, np.float32, np.float64)
+    if isinstance(x1, realval_check) and isinstance(x2, realval_check):
+        x1, x2 = [x1], [x2]
+    
+        
+    x1, x2 = np.array(x1), np.array(x2)
+    mean_x1, mean_x2 = np.mean(x1), np.mean(x2) # if means provided as x1,x2, then these will return the same vals.
+    meandiff = mean_x1 - mean_x2
+    
+    if len(x1)==1 and len(x2)==1:
+        # Then just the means were provided: we need both SDs otherwise error.
+        if not (S_x1 and S_x2):
+            raise ValueError("Requires both of `S_x1` & `S_x2`.")
+        else:
+            from_summary = True
+    else:
+        # Then x1 and x2 are arrays and we proceed as usual
+        from_summary = False
+        S_x1, S_x2 = np.std(x1, ddof=1), np.std(x2, ddof=1)
+        nobs_x1, nobs_x2 = len(x1), len(x2)
+    
+    if ind == True:
+        # Independent samples        
+        if (from_summary == True) and (not (nobs_x1 and nobs_x2)):
+            # Estimating using different pooled SD e.g. https://www.polyu.edu.hk/mm/effectsizefaqs/effect_size_equations2.html
+            pooled_sd = np.sqrt((S_x1**2 + S_x2**2) / 2)
+            print(pooled_sd)
+        else:
+            
+            dof = nobs_x1 + nobs_x2 - 2
+            pooled_sd = np.sqrt( ((nobs_x1 - 1) * S_x1 ** 2 + (nobs_x2 - 1) * S_x2 ** 2) / dof )
+            
+    else:
+        # Related samples
+        if from_summary == True:
+            if not r: 
+                # We need the coefficient otherwise it is biased. Maybe just raise warning and use biased cohens d?
+                raise ValueError("Correlation coefficient `r` required to compute effect size from summary statistics, but was not provided.")
+        else:
+            # Need to compute r from x1 and x2 arrays
+            r = np.corrcoef(x1, x2)[0][1]
+            
+        pooled_sd = np.sqrt(S_x1**2 + S_x2**2 - 2 * r * S_x1 * S_x2)
+        # pooled_sd = np.mean([S_x1, S_x2]) # Hedges' g (average)
+     
+    # Calculate the effect size
+    effectsize = meandiff / pooled_sd
+    
+    if hedges_correction:
+        if (from_summary == True) and (not (nobs_x1 and nobs_x2)):
+            raise ValueError("Requires number of observations from both groups in order to apply Hedges' correction, but these were not provided.")
+        else:
+            if ind == False:
+                # Re-calculate the effect size with the average pooled stdev
+                effectsize = meandiff / np.mean([S_x1, S_x2])
+            
+            effectsize *= (1 - (3 / (4 * (nobs_x1 + nobs_x2) - 9)))
+    
+    return effectsize
+    
 
-d1, g1 = standardised_cohens_d_hedges_g(x1=x1, x2=x2)
-d2, g2 = standardised_cohens_d_hedges_g(t=t, nobs_x1=nobs_x1, nobs_x2=nobs_x2)
-d3, g3 = standardised_cohens_d_hedges_g(t=t, N=nobs_x1 + nobs_x2)
 
-cles = common_language_effect_size(x1=x1,x2=x2)
+# Cohen's d calcs
+d_s_ind = cohens_d(x1, x2)
+d_s_rel = cohens_d(x1, x2, ind=False)
+d_s_ind_summary = cohens_d(np.mean(x1), np.mean(x2), np.std(x1, ddof=1), np.std(x2, ddof=1))
+d_s_rel_summary = cohens_d(np.mean(x1), np.mean(x2), np.std(x1, ddof=1), np.std(x2, ddof=1), r=0.73)
 
-# Test them against the example from the paper
-np.testing.assert_allclose(round(d1, 2), 1.13)
-np.testing.assert_allclose(round(g1, 2), 1.08)
+# Hedges' g correction for Cohen's d
+d_s_ind_g = cohens_d(x1, x2, hedges_correction=True)
+d_s_rel_g = cohens_d(x1, x2, ind=False, hedges_correction=True)
+d_s_ind_summary_g = cohens_d(np.mean(x1), np.mean(x2), np.std(x1, ddof=1), np.std(x2, ddof=1), 
+                             nobs_x1=10, nobs_x2=10, hedges_correction=True)
+d_s_rel_summary_g = cohens_d(np.mean(x1), np.mean(x2), np.std(x1, ddof=1), np.std(x2, ddof=1), 
+                             nobs_x1=10, nobs_x2=10, r=0.73, hedges_correction=True)
 
-np.testing.assert_allclose(round(d2, 2), 1.13)
-np.testing.assert_allclose(round(g2, 2), 1.08)
+# Test them against the examples
+np.testing.assert_allclose(round(d_s_ind, 2), 1.13) # from the paper
+np.testing.assert_allclose(round(d_s_rel, 2), 1.5) # from the paper
+np.testing.assert_allclose(round(d_s_ind_summary, 2), 1.13) # tested with g*power
+np.testing.assert_allclose(round(d_s_rel_summary, 2), 1.13) # tested from here ---> https://www.socscistatistics.com/effectsize/default3.aspx
+np.testing.assert_allclose(round(d_s_ind_g, 2), 1.08) # from the paper
+np.testing.assert_allclose(round(d_s_rel_g, 2), 1.08) # from the paper
+np.testing.assert_allclose(round(d_s_ind_summary_g, 2), 1.08) # tested from here ---> https://www.socscistatistics.com/effectsize/default3.aspx
+np.testing.assert_allclose(round(d_s_rel_summary_g, 2), 1.08) # tested from here ---> https://www.socscistatistics.com/effectsize/default3.aspx
 
-np.testing.assert_allclose(round(d3, 2), 1.13)
-np.testing.assert_allclose(round(g3, 2), 1.08)
 
-print("Testing standardised_cohens_d() for different inputs:")
-print(f"""Using x1 & x2:\n\td = {d1}\n\tg = {g1}\nUsing t, nobs_x1 & nobs_x2:\n\td = {d2}\n\tg = {g2}\nUsing just t and N\n\td = {d3}\n\tg = {g3}""")
-np.testing.assert_allclose(round(cles, 2), 0.79)
-print(f"Testing the CLES:\n\tCLES = {cles}")
+print(f"Cohens d from independent samples data: {d_s_ind}")
+print(f"Cohens d from paired samples data: {d_s_rel}")
+print(f"Cohens d from independent samples data using summary stats: {d_s_ind_summary}")
+print(f"Cohens d from paired samples data using summary stats: {d_s_rel_summary}")
+
+print(f"Hedges g from independent samples data: {d_s_ind_g}")
+print(f"Hedges g from paired samples data: {d_s_rel_g}")
+print(f"Hedges g from independent samples data using summary stats: {d_s_ind_summary_g}")
+print(f"Hedges g from paired samples data using summary stats: {d_s_rel_summary_g}")
+
+print(df.describe())
